2faf293d65f42ef229cc8c6e7e717c4a75d6aae6
ze00 <zerozakiGeek@gmail.com>
2018-07-10 21:10:00 +0800

    update build system
    Signed-off-by: ze00 <zerozakiGeek@gmail.com>

--- build/reg_rule.mk
+++ build/reg_rule.mk
@@ -1,6 +1,7 @@
-$(TARGET):src=$(strip $($(@)_src))
-$(TARGET):out=$(strip $(call fix-name, $(@)))
-$(TARGET):flag=$(strip $(call fix-flag, $(out), $($(@)_flag)))
+$(TARGET):uname=$(call undo-name, $@)
+$(TARGET):src=$(strip $($(uname)_src))
+$(TARGET):out=$(strip $@)
+$(TARGET):flag=$(strip $(call fix-flag, $(out), $($(uname)_flag)))
 $(TARGET):$(DEP)
 	@ echo "$(src) => $@"
 	@ $(CC) -o $(out) $(src) $(CC_FLAG) $(flag)
