14efc6d292d548d994f97f52cca0a580b38da2d7
ze00 <zerozakiGeek@gmail.com>
2018-07-14 14:52:17 +0800

    update build system
    Signed-off-by: ze00 <zerozakiGeek@gmail.com>

--- build/build.mk
+++ build/build.mk
@@ -15,7 +15,9 @@ endef
 define reg_rule
 $(foreach m,$(1), \
 	$(eval TARGET := $(m)) \
-	$(eval DEP := $($(call undo-name, $(m))_src) $(inc) $(MAKEFILE_LIST)) \
+	$(eval DEP := $($(call undo-name, $(m))_src) \
+	$(call fix-name, $($(call undo-name, $(m))_dep)) \
+	$(inc) $(MAKEFILE_LIST)) \
 	$(eval include build/reg_rule.mk))
 endef
 rmodules := $(foreach m, $(MODULE), $(call fix-name, $(m)))
